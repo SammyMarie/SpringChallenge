7cb8a8fd3827da78117e20a8b447afeddbc735c1
Samuel Ifere <sml.ifere@gmail.com>
2016-01-25 22:18:15 +0000

    Refactors config, build.gradle, and SqlConnector files

--- src/main/java/uk/co/sammy/config/ConfigurationMVC.java
+++ src/main/java/uk/co/sammy/config/ConfigurationMVC.java
@@ -3,5 +3,73 @@ package uk.co.sammy.config;
 /**
  * Created by smlif on 25/01/2016.
  */
-public class ConfigurationMVC {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import uk.co.sammy.dao.SqlConnector;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "uk.co.sammy")
+@PropertySource(value = "classpath:local.properties")
+public class ConfigurationMVC extends WebMvcConfigurerAdapter{
+    @Value("${jdbc.driverClassName}")
+    private String dbDriverName;
+
+    @Value("${jdbc.url}")
+    private String dbUrl;
+
+    @Value("${jdbc.username}")
+    private String dbUsername;
+
+    @Value("${jdbc.password}")
+    private String dbPassword;
+
+    @Bean
+    public ViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setViewClass(JstlView.class);
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    private static PropertySourcesPlaceholderConfigurer placeholderConfigurer(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(dbDriverName);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+
+        return dataSource;
+    }
+
+    @Bean
+    public SqlConnector getBookDAO(){
+        return new SqlConnector(getDataSource());
+    }
 }
--- src/main/java/uk/co/sammy/dao/SqlConnector.java
+++ src/main/java/uk/co/sammy/dao/SqlConnector.java
@@ -1,7 +1,15 @@
 package uk.co.sammy.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
 /**
  * Created by smlif on 25/01/2016.
  */
 public class SqlConnector {
+    private JdbcTemplate connector;
+
+    public SqlConnector(DataSource dataSource) {
+        connector = new JdbcTemplate(dataSource);
+    }
 }
